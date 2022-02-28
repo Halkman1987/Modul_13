cf5fbfd4ca2a1167a75474c0da840d20407d05c4
halkman <halk-man@mail.ru>
2022-02-28 11:42:37 +0500

    new

--- Modul_13/Program.cs
+++ Modul_13/Program.cs
@@ -13,36 +13,10 @@ namespace FinalTask
         {
             string Text = File.ReadAllText("C:\\Users\\User\\Desktop\\Text1.txt");
             string[] wordsall = Text.Split(new[] { ' ', ',', ':', '?', '!', '.' }, StringSplitOptions.RemoveEmptyEntries);
-            
-            
+           
             //Не получилось сделать в помощью этого выражения.Может подскажете как его использовать нужно было :)
             var noPunctuationText = new string(Text.Where(c => !char.IsPunctuation(c)).ToArray());
 
-            //подготовим два Списка для добавления
-            List<string> list = new List<string>();
-            LinkedList<string> list2 = new LinkedList<string>();
-
-            Console.WriteLine(" Добавление в List ");
-            var stopWatch = Stopwatch.StartNew();
-            foreach (string word in wordsall)
-            {
-                list.Add(word);
-            }
-            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
-            Console.ReadKey();
-            Console.WriteLine("----------------------------------------------");
-            
-            Console.WriteLine(" Добавление в LinkedList ");
-            var stopWatch2 = Stopwatch.StartNew();
-            foreach (string word in wordsall)
-            {
-                list2.AddFirst(word);
-            }
-            Console.WriteLine(stopWatch2.Elapsed.TotalMilliseconds);
-            Console.ReadKey();
-            
-            
-            
             Console.WriteLine("----Подсчет 10 самых частых слов ----");
             Dictionary<string, int> uniqWord = new Dictionary<string, int>();//словарь для добавление элементов (Слово/Колличество раз) 
             HashSet<string> HashWords = new HashSet<string>(wordsall);//список уникальных слов из текста
