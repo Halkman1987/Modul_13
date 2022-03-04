ae573bf649d3cbdef8182b230070b50857a6382e
Halkman <halk-man@mail.ru>
2022-03-05 00:59:51 +0500

    Задание 13.6.1

--- Modul_13/Program.cs
+++ Modul_13/Program.cs
@@ -11,7 +11,7 @@ namespace FinalTask
     {
         static void Main(string[] args)
         {
-            string Text = File.ReadAllText("C:\\Users\\User\\Desktop\\Text1.txt");
+            string Text = File.ReadAllText("C:\\VB\\file1.txt");
             string[] wordsall = Text.Split(new[] { ' ', ',', ':', '?', '!', '.' }, StringSplitOptions.RemoveEmptyEntries);
             
             
@@ -41,34 +41,6 @@ namespace FinalTask
             Console.WriteLine(stopWatch2.Elapsed.TotalMilliseconds);
             Console.ReadKey();
             
-            
-            
-            Console.WriteLine("----Подсчет 10 самых частых слов ----");
-            Dictionary<string, int> uniqWord = new Dictionary<string, int>();//словарь для добавление элементов (Слово/Колличество раз) 
-            HashSet<string> HashWords = new HashSet<string>(wordsall);//список уникальных слов из текста
-            foreach (var w in HashWords)
-            {
-                int j = 0;
-                foreach (var z in wordsall)
-                {
-                    if (w == z)
-                    {
-                        j++;
-                    }
-                }
-                uniqWord.Add(w, j);
-            }
-            var FinalcountWord = uniqWord.ToArray();
-            for (int i = 0; i < FinalcountWord.Length; i++)
-                for (int j = i; j < FinalcountWord.Length; j++)
-                {
-                    if (FinalcountWord[i].Value < FinalcountWord[j].Value)
-                        (FinalcountWord[i], FinalcountWord[j]) = (FinalcountWord[j], FinalcountWord[i]);
-                }
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(FinalcountWord[i]);
-            }
         }
     }
 }
